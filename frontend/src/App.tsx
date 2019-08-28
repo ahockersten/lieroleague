74d97f880ee7467265209b1de8adc4ffeca3e898
Anders Höckersten <anders@hockersten.se>
2019-08-28 19:51:43 +0200

    Add material ui and typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import Button from '@material-ui/core/Button';
+import React from 'react';
+
+const App: React.FC = () =>
+  <Button variant="contained" color="primary">
+    Hello World
+  </Button>
+
+export default App;
