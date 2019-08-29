4f8b7eef6f85c2ac0e53d9ae3ae530be65e4b2c1
Anders Höckersten <anders@hockersten.se>
2019-08-29 19:41:31 +0200

    Add everything I can think of to player

--- backend/src/player.rs
+++ backend/src/player.rs
@@ -1,28 +1,70 @@
 use serde::{Serialize, Deserialize};
+use eventsourcing::AggregateState;
 use eventsourcing_derive::Event;
 
 #[derive(Debug)]
-pub enum PlayerCommand {
-  Create()
+enum PlayerCommand {
+  Create {
+    real_name: String,
+    email: Email,
+    password: String,
+    salt: String,
+    nick_name: String,
+    color: PlayerColor,
+    nationality: Option<Nationality>,
+    time_zone: Option<i8>,
+    location: Option<Country>,
+    locale: Locale,
+  }
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, Event)]
 #[event_type_version("1.0")]
 #[event_source("events://github.com/ahockersten/lieroleague/player")]
 enum PlayerEvent {
-  PlayerCreated,
+  PlayerCreated {
+    id: u64,
+    real_name: String,
+    email: Email,
+    password: String,
+    salt: String,
+    nick_name: String,
+    color: PlayerColor,
+    nationality: Option<Nationality>,
+    time_zone: Option<i8>,
+    location: Option<Country>,
+    locale: Locale,
+  },
 }
+
+type Email = String;
+type Nationality = String;
+type Country = String;
+type Locale = String;
 
 #[derive(Debug)]
 struct PlayerData {
   id: u64,
-  real_name: Option<String>,
+  real_name: String,
+  email: Email,
+  password: String,
+  salt: String,
   nick_name: String,
   color: PlayerColor,
-
+  nationality: Option<Nationality>,
+  time_zone: Option<i8>,
+  location: Option<Country>,
+  locale: Locale,
+  generation: u64,
 }
 
-#[derive(Debug)]
+impl AggregateState for PlayerData {
+  fn generation(&self) -> u64 {
+    self.generation
+  }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
 struct PlayerColor {
   r: u8,
   g: u8,
