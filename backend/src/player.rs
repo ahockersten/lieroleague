9e11bee8e5d709b93376fc3f530b0a6a63db5e9c
Anders Höckersten <anders@hockersten.se>
2019-08-29 20:29:42 +0200

    Implement player create message

--- backend/src/player.rs
+++ backend/src/player.rs
@@ -1,6 +1,8 @@
 use serde::{Serialize, Deserialize};
-use eventsourcing::AggregateState;
+use eventsourcing::{Aggregate, AggregateState, Error};
 use eventsourcing_derive::Event;
+use uuid::Uuid;
+use passwords::hasher;
 
 #[derive(Debug)]
 enum PlayerCommand {
@@ -8,26 +10,6 @@ enum PlayerCommand {
     real_name: String,
     email: Email,
     password: String,
-    salt: String,
-    nick_name: String,
-    color: PlayerColor,
-    nationality: Option<Nationality>,
-    time_zone: Option<i8>,
-    location: Option<Country>,
-    locale: Locale,
-  }
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone, Event)]
-#[event_type_version("1.0")]
-#[event_source("events://github.com/ahockersten/lieroleague/player")]
-enum PlayerEvent {
-  PlayerCreated {
-    id: u64,
-    real_name: String,
-    email: Email,
-    password: String,
-    salt: String,
     nick_name: String,
     color: PlayerColor,
     nationality: Option<Nationality>,
@@ -37,6 +19,59 @@ enum PlayerEvent {
   },
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone, Event)]
+#[event_type_version("1.0")]
+#[event_source("events://github.com/ahockersten/lieroleague/player")]
+enum PlayerEvent {
+  Created {
+    id: Uuid,
+    real_name: String,
+    email: Email,
+    password: [u8; 24],
+    salt: [u8; 16],
+    nick_name: String,
+    color: PlayerColor,
+    nationality: Option<Nationality>,
+    time_zone: Option<i8>,
+    location: Option<Country>,
+    locale: Locale,
+  },
+}
+
+impl From<PlayerCommand> for PlayerEvent {
+  fn from(source: PlayerCommand) -> Self {
+    match source {
+      PlayerCommand::Create {
+        real_name,
+        email,
+        password,
+        nick_name,
+        color,
+        nationality,
+        time_zone,
+        location,
+        locale
+      } => {
+        let salt = hasher::gen_salt();
+        let salted_password = hasher::bcrypt(12, &salt, &password);
+        PlayerEvent::Created {
+          id: Uuid::new_v4(),
+          real_name,
+          email,
+          password: salted_password.unwrap(), // TODO: this could fail
+          salt: salt,
+          nick_name,
+          color,
+          nationality,
+          time_zone,
+          location,
+          locale
+        }
+      }
+    }
+  }
+}
+
 type Email = String;
 type Nationality = String;
 type Country = String;
@@ -44,11 +79,11 @@ type Locale = String;
 
 #[derive(Debug)]
 struct PlayerData {
-  id: u64,
+  id: Uuid,
   real_name: String,
   email: Email,
-  password: String,
-  salt: String,
+  password: [u8; 24],
+  salt: [u8; 16],
   nick_name: String,
   color: PlayerColor,
   nationality: Option<Nationality>,
@@ -70,3 +105,50 @@ struct PlayerColor {
   g: u8,
   b: u8
 }
+
+struct Player;
+
+impl Aggregate for Player {
+  type Event = PlayerEvent;
+  type Command = PlayerCommand;
+  type State = PlayerData;
+
+  fn apply_event(state: &Self::State, evt: Self::Event) -> Result<Self::State, Error> {
+    let data = match evt {
+      PlayerEvent::Created {
+        id,
+        real_name,
+        email,
+        password,
+        salt,
+        nick_name,
+        color,
+        nationality,
+        time_zone,
+        location,
+        locale
+      } => PlayerData {
+        id,
+        real_name,
+        email,
+        password,
+        salt,
+        nick_name,
+        color,
+        nationality,
+        time_zone,
+        location,
+        locale,
+        generation: state.generation + 1,
+      },
+    };
+    Ok(data)
+  }
+
+  fn handle_command(_state: &Self::State, cmd: Self::Command) -> Result<Vec<Self::Event>, Error> {
+        // SHOULD DO: validate state and command
+
+        // if validation passes...
+        Ok(vec![cmd.into()])
+      }
+    }
