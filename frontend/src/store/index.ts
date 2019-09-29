43fabc2f50a09ae58d7c5612587b8274dfeabd45
Anders Höckersten <anders@hockersten.se>
2019-09-29 15:56:03 +0200

    Add redux-logger

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { createLogger } from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
@@ -9,7 +10,7 @@ import reducers from '../reducers';
 const sagaMiddleware = createSagaMiddleware();
 
 /** Create middlewares for redux */
-let middlewares = applyMiddleware(sagaMiddleware);
+let middlewares = applyMiddleware(sagaMiddleware, createLogger());
 
 /** Create redux store */
 const store = createStore(
