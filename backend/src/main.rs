9e1167c329ae00a1b282eb239c8bc9df93e9aab2
Anders Höckersten <anders@hockersten.se>
2019-09-29 12:20:10 +0200

    Add CORS support to backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -7,15 +7,37 @@ extern crate lieroleague;
 use lieroleague::db;
 use lieroleague::player;
 use lieroleague::state;
+use rocket::http::Method;
+use rocket_cors;
+use rocket_cors::{AllowedOrigins, Error};
 
 #[get("/")]
 fn index() -> () {}
 
-fn main() {
+fn main() -> Result<(), Error> {
+    let allowed_origins = AllowedOrigins::some_exact(&["http://localhost:3000"]);
+    let allowed_methods = vec![Method::Get, Method::Post]
+        .into_iter()
+        .map(From::from)
+        .collect();
+    let cors = rocket_cors::CorsOptions {
+        allowed_origins,
+        allowed_methods,
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()?;
+
+    // FIXME you should be able to initialize state here, if you can initialize the db
+    // here as well?
+
     rocket::ignite()
         .attach(db::LieroLeagueDb::fairing())
         .manage(state::State::default())
         .mount("/", routes![index])
         .mount("/player", player::routes())
+        .attach(cors)
         .launch();
+
+    Ok(())
 }
