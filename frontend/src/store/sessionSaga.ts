4dfae2cccc2ef10063269cafbeb4dea0a7207df7
Anders Höckersten <anders@hockersten.se>
2019-10-05 09:26:42 +0200

    Replace axios with ky
    Axios seems to be badly maintained, fetch() is supported for all
    reasonable browsers, and ky seems like a nice simple wrapper around
    fetch().

--- frontend/src/store/sessionSaga.ts
+++ frontend/src/store/sessionSaga.ts
@@ -2,42 +2,27 @@ import { SagaIterator } from '@redux-saga/core';
 import { takeLatest, call, put } from 'redux-saga/effects';
 import { Actions, LoginAction, PlayerLoginData } from '../actions';
 import { apiClient } from './index';
-import { PlayerProfile } from '../reducers/playerProfile.reducer';
 
-function loginApi(playerLoginData: PlayerLoginData): Promise<void> {
-  return apiClient.request({
-    method: 'post',
-    url: '/player/login',
-    data: playerLoginData
+function loginApi(playerLoginData: PlayerLoginData): Promise<Response> {
+  return apiClient.post('player/login', {
+    json: playerLoginData
   });
 }
 
-function getProfileApi(): Promise<PlayerProfile> {
-  return apiClient.request({
-    method: 'get',
-    url: '/player/profile'
-  });
+function getProfileApi(): Promise<Response> {
+  return apiClient.get('player/profile');
 }
 
 function* loginEffectSaga(action: LoginAction): SagaIterator {
-  try {
-    yield call(loginApi, action.payload);
-    yield put({ type: Actions.WATCH_GET_PROFILE });
-  } catch (e) {
-    // catch error on a bad axios call
-    // alert using an alert library
-  }
+  yield call(loginApi, action.payload);
+  yield put({ type: Actions.WATCH_GET_PROFILE });
 }
 
 function* getProfileEffectSaga(): SagaIterator {
-  try {
-    const { data } = yield call(getProfileApi);
+  const response = yield call(getProfileApi);
+  const json = yield response.json();
 
-    yield put({ type: Actions.UPDATE_PROFILE, payload: data });
-  } catch (e) {
-    // catch error on a bad axios call
-    // alert using an alert library
-  }
+  yield put({ type: Actions.UPDATE_PROFILE, payload: json });
 }
 
 export function* watchLoginSaga(): SagaIterator {
